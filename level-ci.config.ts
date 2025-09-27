e188be89cd019b6d3204457ed0975f05556d9e6b
Sukhada Pawar <sukhada.pawar@levelaccess.com>
2025-09-27 10:21:13 -0400

    new PR

---
+++ level-ci.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from '@userway/cicd-cli' 
+export default { 
+  organization: "acme-kuuzi",
+  project: "acme-business-site",
+  token: "eaykr3grjmpnfok4tyucm3i456jtr1qijvxgpnrq",
+  reportPaths: ["./la-a11y-reports"],
+  server: "https://api.qa.userway.dev",
+
+} satisfies Config;
